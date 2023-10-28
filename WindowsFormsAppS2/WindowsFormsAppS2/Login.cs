c399bcb53bf7eae4f7f20a84838b6a6f50f95a1a
Abdellah Abdelkafi <60241139+xxvista@users.noreply.github.com>
2023-10-28 19:28:32 +0100

    Add files via upload

---
+++ WindowsFormsAppS2/WindowsFormsAppS2/Login.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppS2
+{
+    public partial class Login : Form
+    {
+        //[DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        //private static extern IntPtr CreateRoundRectRgn
+        //(
+        //    int nLeft,     // x-coordinate of upper-left corner
+        //    int nTop,      // y-coordinate of upper-left corner
+        //    int nRight,    // x-coordinate of lower-right corner
+        //    int nBottom,   // y-coordinate of lower-right corner
+        //    int nWidthEllipse, // height of ellipse
+        //    int nHeightEllipse // width of ellipse
+        //);
+
+        public string u = "abdellah", p = "p@ss";
+        public Login()
+        {
+            InitializeComponent();
+            int borderRadius = 9; // Adjust this value to control the roundness
+
+            // Round the btnConnect button
+            RoundControl(btnConnect, borderRadius);
+
+            // Round the btnFermmer button
+            RoundControl(btnFermmer, borderRadius);
+        }
+
+        private void connect_Click(object sender, EventArgs e)
+        {
+            login();
+        }
+
+        private void tbUser_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                login();
+            }
+        }
+
+        private void login()
+        {
+            if (tbUser.Text == u && tbPass.Text == p)
+            {
+                this.Hide();
+                Home h = new Home();
+                h.Show();
+            }
+            else
+            {
+                MessageBox.Show("Le nom d'utilisateur ou le mot de pass est incorrect");
+            }
+        }
+
+        
+
+        private void btnFermmer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+
+                tbPass.PasswordChar = (char)0;
+            else
+                tbPass.PasswordChar = (char)'*';
+
+        }
+
+        private void btnSignUp_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            SignUp s = new SignUp();
+            s.Show();
+        }
+
+        private void RoundControl(Control control, int borderRadius)
+        {
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+            Rectangle bounds = control.ClientRectangle;
+            int radius = borderRadius;
+            path.AddArc(bounds.Left, bounds.Top, 2 * radius, 2 * radius, 180, 90);
+            path.AddArc(bounds.Right - 2 * radius, bounds.Top, 2 * radius, 2 * radius, 270, 90);
+            path.AddArc(bounds.Right - 2 * radius, bounds.Bottom - 2 * radius, 2 * radius, 2 * radius, 0, 90);
+            path.AddArc(bounds.Left, bounds.Bottom - 2 * radius, 2 * radius, 2 * radius, 90, 90);
+            path.CloseFigure();
+            control.Region = new Region(path);
+        }
+
+        //private void btnConnect_MouseEnter(object sender, EventArgs e)
+        //{
+        //    // Customize button appearance when the mouse enters
+        //    btnConnect.FlatStyle = FlatStyle.Flat;
+        //    btnConnect.FlatAppearance.BorderSize = 2;
+        //    btnConnect.FlatAppearance.BorderColor = Color.DarkGray;
+        //    btnConnect.FlatAppearance.MouseDownBackColor = Color.LightGray;
+        //    btnConnect.FlatAppearance.MouseOverBackColor = Color.WhiteSmoke;
+        //}
+
+        //private void btnConnect_MouseLeave(object sender, EventArgs e)
+        //{
+        //    // Restore the default appearance when the mouse leaves
+        //    btnConnect.FlatStyle = FlatStyle.Standard;
+        //}
+    }
+}
---
+++ WindowsFormsAppS2/WindowsFormsAppS2/SignUp.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WindowsFormsAppS2
+{
+    partial class SignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSignUp = new System.Windows.Forms.Button();
+            this.cb1 = new System.Windows.Forms.CheckBox();
+            this.btnFermmer = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.cb2 = new System.Windows.Forms.CheckBox();
+            this.tbCPass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(103)))), ((int)(((byte)(127)))));
+            this.btnSignUp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSignUp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSignUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSignUp.ForeColor = System.Drawing.Color.White;
+            this.btnSignUp.Location = new System.Drawing.Point(246, 255);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(160, 48);
+            this.btnSignUp.TabIndex = 15;
+            this.btnSignUp.Text = "Sign Up";
+            this.btnSignUp.UseVisualStyleBackColor = false;
+            this.btnSignUp.Click += new System.EventHandler(this.btnSignUp_Click);
+            // 
+            // cb1
+            // 
+            this.cb1.AutoSize = true;
+            this.cb1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(228)))), ((int)(((byte)(232)))));
+            this.cb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb1.Location = new System.Drawing.Point(650, 135);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(18, 17);
+            this.cb1.TabIndex = 14;
+            this.cb1.UseVisualStyleBackColor = false;
+            this.cb1.CheckedChanged += new System.EventHandler(this.cb1_CheckedChanged);
+            // 
+            // btnFermmer
+            // 
+            this.btnFermmer.BackColor = System.Drawing.Color.IndianRed;
+            this.btnFermmer.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFermmer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFermmer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFermmer.ForeColor = System.Drawing.Color.White;
+            this.btnFermmer.Location = new System.Drawing.Point(336, 334);
+            this.btnFermmer.Name = "btnFermmer";
+            this.btnFermmer.Size = new System.Drawing.Size(160, 48);
+            this.btnFermmer.TabIndex = 13;
+            this.btnFermmer.Text = "Fermmer";
+            this.btnFermmer.UseVisualStyleBackColor = false;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(103)))), ((int)(((byte)(127)))));
+            this.btnConnect.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.ForeColor = System.Drawing.Color.White;
+            this.btnConnect.Location = new System.Drawing.Point(433, 255);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(160, 48);
+            this.btnConnect.TabIndex = 12;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = false;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // tbPass
+            // 
+            this.tbPass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(228)))), ((int)(((byte)(232)))));
+            this.tbPass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPass.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(72)))), ((int)(((byte)(76)))));
+            this.tbPass.Location = new System.Drawing.Point(336, 125);
+            this.tbPass.Name = "tbPass";
+            this.tbPass.PasswordChar = '*';
+            this.tbPass.Size = new System.Drawing.Size(358, 34);
+            this.tbPass.TabIndex = 11;
+            // 
+            // tbUser
+            // 
+            this.tbUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(228)))), ((int)(((byte)(232)))));
+            this.tbUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUser.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(72)))), ((int)(((byte)(76)))));
+            this.tbUser.Location = new System.Drawing.Point(336, 69);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(358, 34);
+            this.tbUser.TabIndex = 10;
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPass.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(28)))), ((int)(((byte)(29)))));
+            this.lblPass.Location = new System.Drawing.Point(110, 128);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(120, 29);
+            this.lblPass.TabIndex = 9;
+            this.lblPass.Text = "Password";
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(28)))), ((int)(((byte)(29)))));
+            this.lblUser.Location = new System.Drawing.Point(110, 69);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(120, 29);
+            this.lblUser.TabIndex = 8;
+            this.lblUser.Text = "Utilisateur";
+            // 
+            // cb2
+            // 
+            this.cb2.AutoSize = true;
+            this.cb2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(228)))), ((int)(((byte)(232)))));
+            this.cb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb2.Location = new System.Drawing.Point(650, 175);
+            this.cb2.Name = "cb2";
+            this.cb2.Size = new System.Drawing.Size(18, 17);
+            this.cb2.TabIndex = 18;
+            this.cb2.UseVisualStyleBackColor = false;
+            this.cb2.CheckedChanged += new System.EventHandler(this.cb2_CheckedChanged);
+            // 
+            // tbCPass
+            // 
+            this.tbCPass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(228)))), ((int)(((byte)(232)))));
+            this.tbCPass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbCPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCPass.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(72)))), ((int)(((byte)(76)))));
+            this.tbCPass.Location = new System.Drawing.Point(336, 165);
+            this.tbCPass.Name = "tbCPass";
+            this.tbCPass.PasswordChar = '*';
+            this.tbCPass.Size = new System.Drawing.Size(358, 34);
+            this.tbCPass.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(28)))), ((int)(((byte)(29)))));
+            this.label1.Location = new System.Drawing.Point(110, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 29);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Confirm password";
+            // 
+            // SignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cb2);
+            this.Controls.Add(this.tbCPass);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSignUp);
+            this.Controls.Add(this.cb1);
+            this.Controls.Add(this.btnFermmer);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.tbPass);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblUser);
+            this.Name = "SignUp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sign Up";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSignUp;
+        private System.Windows.Forms.CheckBox cb1;
+        private System.Windows.Forms.Button btnFermmer;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.CheckBox cb2;
+        private System.Windows.Forms.TextBox tbCPass;
+        private System.Windows.Forms.Label label1;
+    }
+}
